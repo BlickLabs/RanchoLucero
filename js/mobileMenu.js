37f02baca93b04a8b78fa8029998db87528de62f
MitchelHdz <sebastianvicky_@hotmail.com>
2016-05-26 17:12:40 -0500

    mobileMenu.js indentation changed

--- js/mobileMenu.js
+++ js/mobileMenu.js
@@ -1,7 +1,7 @@
-      $(document).ready(function() {
-      $(document, '#js-bootstrap-offcanvas').on('click, touch', function(){ 
-           if(!$('#js-bootstrap-offcanvas').hasClass('in')){
-              $('#js-bootstrap-offcanvas').removeClass('in')
-           }
-      });
+$(document).ready(function() {
+    $(document, '#js-bootstrap-offcanvas').on('click, touch', function(){ 
+      if(!$('#js-bootstrap-offcanvas').hasClass('in')){
+        $('#js-bootstrap-offcanvas').removeClass('in')
+      }
     });
+});
